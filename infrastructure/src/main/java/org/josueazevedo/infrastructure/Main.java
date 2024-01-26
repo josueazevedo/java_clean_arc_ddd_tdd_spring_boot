3d8f0952ae4ae50114fd988d54a4c55c721b52dc
Josué Azevedo <josue-azevedo@hotmail.com>
2024-01-26 15:27:52 -0300

    chore: add spring boot config

--- infrastructure/src/main/java/org/josueazevedo/infrastructure/Main.java
+++ infrastructure/src/main/java/org/josueazevedo/infrastructure/Main.java
@@ -1,7 +1,18 @@
 package org.josueazevedo.infrastructure;
 
+import org.josueazevedo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
+
+@SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+//        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "development");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        SpringApplication.run(WebServerConfig.class, args);
+        //env SPRING_PROFILES_ACTIVE=production //POD
     }
 }
